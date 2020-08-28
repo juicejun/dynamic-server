8584f30d85579196ff09b8a11393c1f6a22ef72c
juicejun <827387950@qq.com>
2020-08-28 12:42:58 +0800

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+//读数据库
+const usersString = fs.readFileSync('./db/users.json').toString()
+const usersArray = JSON.parse(usersString)
+
+//写数据库
+const users3 = {id:3,name:'tom',password:'zzz'}
+usersArray.push(users3)
+const string = JSON.stringify(usersArray)
+fs.writeFileSync('./db/users.json',string)
